a7d625221f6927967d989ef367c4c92e005f6fa7
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-08-15 16:48:14 -0300

    Update index.js

--- Jaydem_Jaymes/index.js
+++ Jaydem_Jaymes/index.js
@@ -10,7 +10,7 @@ function multiplicador() {
     ];
 
     // Array com os pesos das imagens para seleção ponderada
-    var pesos = [5, 4, 1];
+    var pesos = [5, 1, 1];
 
     // Array com os multiplicadores de ganhos para cada imagem
     var multiplicadores = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
